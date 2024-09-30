ef9ef121b873279c0db2791aa15c5a828daaa072
hiphop200199 <hiphop200199@gmail.com>
2024-10-01 01:14:03 +0800

    create components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { ReserveLinksComponent } from "./reserve-links/reserve-links.component";
+import { NewSuvComponent } from "./new-suv/new-suv.component";
+import { NewSedanComponent } from "./new-sedan/new-sedan.component";
+import { NewEvComponent } from "./new-ev/new-ev.component";
+import { NewGearComponent } from "./new-gear/new-gear.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, ReserveLinksComponent, NewSuvComponent, NewSedanComponent, NewEvComponent, NewGearComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
