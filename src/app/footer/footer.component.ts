dec8e280830c4c1ecd7b92c32dc163a0b5ec686e
hiphop200199 <hiphop200199@gmail.com>
2024-10-02 14:30:06 +0800

    done.

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, QueryList, Renderer2, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -7,6 +7,28 @@ import { Component } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {
-
+export class FooterComponent implements AfterViewInit{
+  @ViewChildren('linkBox',{read:ElementRef<HTMLDivElement>}) linkBoxes! :QueryList<ElementRef<HTMLDivElement>>;
+  @ViewChildren('linkBtn',{read:ElementRef<HTMLButtonElement>}) linkBtns!:QueryList<ElementRef<HTMLButtonElement>>;
+  linkBoxId:string[] = ['know-more','download','other'];
+  linkBoxArray!:ElementRef<HTMLDivElement>[] ;
+  linkBtnsArray!:ElementRef<HTMLButtonElement>[];
+  ngAfterViewInit(): void {
+   this.linkBoxArray = this.linkBoxes.toArray();
+   this.linkBtnsArray = this.linkBtns.toArray();
+  }   
+  toggleLinkBox(id:string):void{
+  let index:number = this.linkBoxArray.findIndex(b => b.nativeElement.id===id);
+  let btn:ElementRef<HTMLButtonElement> = this.linkBtnsArray[index];
+  let box:ElementRef<HTMLDivElement> = this.linkBoxArray[index];
+  btn.nativeElement.classList.toggle('active');
+  box.nativeElement.classList.toggle('active');
+   this.linkBoxArray.forEach( (b,index) => {
+    if(b.nativeElement.id!==id){
+      this.renderer.removeClass(b.nativeElement,'active');
+      this.renderer.removeClass(this.linkBtnsArray[index].nativeElement,'active');
+    }
+   })
+  }
+  constructor(private renderer: Renderer2){}
 }
